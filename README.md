18ae1a6af364635eee1af3cb1a714b25e70c600e
aanohin <artyom.anohin@pendevops.net>
2018-08-22 13:33:21 +0300

    Update pooling; Add response verification; Add readme

--- README.md
+++ README.md
@@ -27,3 +27,10 @@ Start vport creation by executing:
 ```bash
 http :9001/api/vport foo=bar
 ```
+
+Start load testing by executing:
+```bash
+mvn -pl load-test gatling:test
+```
+
+![ConstantUsersPerSec](1-ConstantUsersPerSec_15-Sec.png)
--- load-test/src/test/scala/io/github/q1nt/load/CamundaSimulation.scala
+++ load-test/src/test/scala/io/github/q1nt/load/CamundaSimulation.scala
@@ -1,11 +1,16 @@
 package io.github.q1nt.load
 
+
 import java.util.UUID
 
+import com.typesafe.scalalogging.LazyLogging
 import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
 import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
+import scala.concurrent.duration._
 
-class CamundaSimulation extends Simulation {
+class CamundaSimulation extends Simulation with LazyLogging {
   val httpProtocol = http
     .baseURL("http://172.18.195.61:9001")
     .inferHtmlResources()
@@ -16,31 +21,43 @@ class CamundaSimulation extends Simulation {
     "accept-encoding" -> "gzip, deflate",
     "cache-control" -> "no-cache")
 
-  var randomId: String = ""
+  private val createVPort: HttpRequestBuilder = http("Create vPort")
+    .post("/api/vport")
+    .headers(headers_0)
+    .body(StringBody(session => s"""{ "foo":"${session("randomUUID").as[String]}" }""")).asJSON
 
-  var scn = scenario("Provisioning vPort")
-    .exec(session => {
-      randomId = UUID.randomUUID().toString
-      session
-    })
-    .exec(http("Create vPort")
-      .post("/api/vport")
-      .headers(headers_0)
-      .body(StringBody(session => s"""{ "foo": "${randomId}" }""")).asJSON
-      .check(status is 202))
-    .pause(15)
-    .exec(http("Get vPoer")
-      .get(session => s"/api/vport/${ randomId}")
-      .headers(headers_0)
-      .check(
-        jsonPath("$.status").saveAs("status")
-      ))
-    .exec(session => {
-      println(s"=========================!!!!!!!vPort ${randomId} Status!!!!!!!===========================")
-      println(session("status").as[String])
-      println("=========================\n")
-      session
-    })
+  private val pollProvisioningState = asLongAs(session => session("vPortStatus").as[String] == "creating")(
+    pause(1000.millis)
+      .exec(
+        http("Check vPort status")
+          .get(session => s"/api/vport/${session("randomUUID").as[String]}")
+          .check(jsonPath("$.status").saveAs("vPortStatus"))
+      )
+      .exec(session => {
+        logger.debug(s"\n===========>>>>> vPort ${session("randomUUID").as[String]} status: ${session("vPortStatus").as[String]} <<<<<===========\n")
+        session
+      })
+  )
 
-  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+  private val validateVPortStatus: HttpRequestBuilder = http("Validate vPort")
+    .get(session => s"/api/vport/${session("randomUUID").as[String]}")
+    .check(jsonPath("$.status").is("deployed"))
+
+  val scn = scenario("Camunda POC Simulation")
+    // Create vPort
+    .exec(session => session.set("vPortStatus", "creating"))
+    .exec(session => session.set("randomUUID", UUID.randomUUID().toString))
+    .exec(createVPort)
+    .exec(pollProvisioningState)
+    .exec(validateVPortStatus)
+
+  //Add 10 user immediately
+  setUp(scn.inject(atOnceUsers(10))).protocols(httpProtocol)
+
+  //Add users from 1 to 10 during 15 seconds
+  //setUp(scn.inject(rampUsersPerSec(1) to 10 during (15 seconds))).protocols(httpProtocol)
+
+  //Add 1 user per second during 15 seconds
+  //setUp(scn.inject(constantUsersPerSec(1) during (15 seconds))).protocols(httpProtocol)
+
 }
